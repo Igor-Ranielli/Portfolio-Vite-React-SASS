fc42f4ab7ce5540d8373bc98de11c14058059bce
Igor-Ranielli <igorranielli@hotmail.com>
2023-07-17 19:47:02 -0300

    Added a new project

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,11 +3,13 @@ import '../styles/components/projectscontainer.sass'
 import img1 from '../img/Projeto 1.png'
 import img2 from '../img/Projeto 2.png'
 import img3 from '../img/Projeto 3.png'
+import img4 from '../img/Projeto 4.png'
 
 const projects = [
     {id: "pokenext", name: "Pokedex feito com Next.JS", image: <img src={img1} alt="Pokedex com next.js"></img>, link: "https://github.com/Igor-Ranielli/pokenext"},
     {id: "buscapreco", name: "Clone do BuscaPé feito com Django e Selenium", image: <img src={img2} alt="BuscaPreço com Django E Selenium"></img>, link: "https://github.com/Igor-Ranielli/BuscaPrecoFrontEnd"},
     {id: "calculamedia", name: "Calculadora de Média da UNIP feita com Python", image: <img src={img3} alt="Calculadora de Média UNIP"></img>, link: "https://github.com/Igor-Ranielli/Calcula-Media-Python"},
+    {id: "reactmovieslib", name: "Projeto com React, consumindo a API do TMDB (filmes), utilizando hooks e React Router, criado com Vite.", image: <img src={img4} alt="React Movies Lib"></img>, link: "https://igor-ranielli.github.io/React-Movies-Lib"}
 ]
 
 const ProjectsContainer = () => {
