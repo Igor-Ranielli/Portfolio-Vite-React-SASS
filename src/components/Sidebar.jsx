24c9135731a3934ad321185bbbff5a83ab79b84d
Igor-Ranielli <igorranielli@hotmail.com>
2023-08-01 14:06:16 -0300

    Version 1.3 New Download Link

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,21 @@
 import SocialNetworks from './SocialNetworks';
+import InformationContainer from './InformationContainer';
 
 import Avatar from '../img/eu.jpg';
 
 import '../styles/components/sidebar.sass';
-import InformationContainer from './InformationContainer';
+
 
 const Sidebar = () => { 
-    return <aside id="sidebar">
+    return (
+    <aside id="sidebar">
         <img src={Avatar} alt="Igor Ranielli"/>
         <p className="title">Desenvolvedor</p>
         <SocialNetworks />
         <InformationContainer />
-        <a href="https://drive.google.com/uc?export=download&id=1kPUyeZYS2eZ8hHVmQgcus8C1MkViYkBh" className="btn" role="button">Download Currículo</a>
-    </aside>;
+        <a href="https://drive.google.com/uc?export=download&id=1cOXzfc15ggxV2plSxErX3HYQC6xO29lF" className="btn" role="button">Download Currículo</a>
+    </aside>
+    );
 };
 
 export default Sidebar;
